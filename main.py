34ced5c2bed0ea8ca3bed85e34e93fe719163f42
jobe2208 <jobe2208@student.miun.se>
2026-04-14 13:59:39 +0200

    Normalized release dates to YYYY

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from bokeh.plotting import figure, show
 from bokeh.layouts import row
 from bokeh.models import ColumnDataSource, HoverTool
 from view1 import plot_data
+from view2 import hidden_gems
+
 def main():
     imdb_data = pd.read_csv("imdb_movie_metadata.csv")
     rotten_tomatoes_data = pd.read_csv("movie_info.csv")
@@ -12,9 +14,12 @@ def main():
     imdb_data = imdb_data.drop_duplicates()
     rotten_tomatoes_data = rotten_tomatoes_data.drop_duplicates()
 
-    plot_data(imdb_data, rotten_tomatoes_data)
+    rotten_tomatoes_data['release_date'] = pd.to_numeric(rotten_tomatoes_data['release_date'].astype(str).str[-4:],
+                                                         errors='coerce').astype('Int64')
+    rotten_tomatoes_data = rotten_tomatoes_data.dropna(subset=['release_date'])
+
+    #plot_data(imdb_data, rotten_tomatoes_data)
+    hidden_gems(imdb_data, rotten_tomatoes_data)
 
 if __name__ == "__main__":
     main()
-
-#I might do some edits here later
