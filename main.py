b93aef7a037d410b418145f69a6b4ee0f479191c
jobe2208 <jobe2208@student.miun.se>
2026-04-15 11:29:04 +0200

    Normalization of dates Updated version

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def main():
     rotten_tomatoes_data = pd.read_csv("movie_info.csv")
     # Remove rows with missing values and duplicates
     imdb_data = imdb_data.dropna()
+
+    #Normalizethe format
+    rotten_tomatoes_data['release_date'] = pd.to_numeric(rotten_tomatoes_data['release_date'].astype(str).str[-4:],
+                                                         errors='coerce').astype('Int64')
     rotten_tomatoes_data = rotten_tomatoes_data.dropna()
     imdb_data = imdb_data.drop_duplicates()
     rotten_tomatoes_data = rotten_tomatoes_data.drop_duplicates()
-
-    rotten_tomatoes_data['release_date'] = pd.to_numeric(rotten_tomatoes_data['release_date'].astype(str).str[-4:],
-                                                         errors='coerce').astype('Int64')
-    rotten_tomatoes_data = rotten_tomatoes_data.dropna(subset=['release_date'])
 
     #plot_data(imdb_data, rotten_tomatoes_data)
     hidden_gems(imdb_data, rotten_tomatoes_data)
